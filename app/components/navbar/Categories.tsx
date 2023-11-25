38ad97bd0650fe1ffb9c8e8e80101c91e73b90e8
ashiqYousuf <ashiqhussainkumar143@gmail.com>
2023-11-25 22:33:04 +0530

    Added categories ui to the airbnb clone

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -1,8 +1,11 @@
 "use client"
 
 import Container from "../Container";
-import { TbBeach } from "react-icons/tb";
-import { GiWindmill } from "react-icons/gi";
+import { TbBeach, TbMountain, TbPool } from "react-icons/tb";
+import { GiBarn, GiBoatFishing, GiCactus, GiCastle, GiCaveEntrance, GiForestCamp, GiIsland, GiWindmill } from "react-icons/gi";
+import { FaSkiing } from "react-icons/fa";
+import { IoDiamond } from "react-icons/io5";
+import { BsSnow } from "react-icons/bs";
 import { MdOutlineVilla } from "react-icons/md";
 import CategoryBox from "../CategoryBox";
 import { usePathname, useSearchParams } from "next/navigation";
@@ -22,7 +25,67 @@ export const categories = [
         label: 'Modern',
         icon: MdOutlineVilla,
         description: "This property is modern!"
-    }
+    },
+    {
+        label: 'Countryside',
+        icon: TbMountain,
+        description: "This property is in the countryside!"
+    },
+    {
+        label: 'Pools',
+        icon: TbPool,
+        description: "This property has a pool!"
+    },
+    {
+        label: 'Islands',
+        icon: GiIsland,
+        description: "This property is on an island!"
+    },
+    {
+        label: 'Lake',
+        icon: GiBoatFishing,
+        description: "This property is close to a lake!"
+    },
+    {
+        label: 'Skiing',
+        icon: FaSkiing,
+        description: "This property has skiing activities!"
+    },
+    {
+        label: 'Castles',
+        icon: GiCastle,
+        description: "This property is in a castle!"
+    },
+    {
+        label: 'Camping',
+        icon: GiForestCamp,
+        description: "This property has camping activities!"
+    },
+    {
+        label: 'Arctic',
+        icon: BsSnow,
+        description: "This property has snowing activities!"
+    },
+    {
+        label: 'Cave',
+        icon: GiCaveEntrance,
+        description: "This property is in a cave!"
+    },
+    {
+        label: 'Desert',
+        icon: GiCactus,
+        description: "This property is in the desert!"
+    },
+    {
+        label: 'Barns',
+        icon: GiBarn,
+        description: "This property is in the barn!"
+    },
+    {
+        label: 'Lux',
+        icon: IoDiamond,
+        description: "This property is luxurious!"
+    },
 ]
 
 // Show Categories only on the Index page i.e '/'
