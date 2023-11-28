41ee02cdbe9de0d8e915269026f3b03cd901c663
ashiqYousuf <ashiqhussainkumar143@gmail.com>
2023-11-28 21:31:51 +0530

    enabled listing detail route

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,4 +1,13 @@
-import { User } from "@prisma/client";
+import { Listing, User } from "@prisma/client";
+
+// We are sanitizing data by not sending Date objects to the client components!
+
+export type SafeListing = Omit<
+    Listing,
+    "createdAt"
+> & {
+    createdAt: string;
+}
 
 export type SafeUser = Omit<
     User,
